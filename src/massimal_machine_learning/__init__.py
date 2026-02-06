9887ab00f398ea31e9406569e4a91ca9a6be799d
Martin H. Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2026-02-06 13:11:21 +0100

    Add __init__ to mark folder as Python package

---
+++ src/massimal_machine_learning/__init__.py
@@ -0,0 +1,6 @@
+# This file could be used to expose a public API for the massimal_machine_learning
+# package. For example, key functions or classes could be imported here to make them
+# available when users import the package. However, it is currently empty, so users
+# need to import specific modules or functions directly from their respective
+# submodules.
+
